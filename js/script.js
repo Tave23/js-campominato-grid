a299c9cf2770d26a7beea03d28446217c725aee0
Davide Tavella <tavella01@gmail.com>
2021-11-02 16:52:51 +0100

    dimensioni tabella

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
 
 const container = document.querySelector('.container');
+// const tabella = document.querySelector('.container');
 
-for(i = 0; i < 64; i++){
+// init(tabella);
+
+// function init(tabella){
    
-   const quadratino = createSquare(container);
-
-   quadratino.addEventListener('click',function() {
-      console.log(this);
-      this.classList.add('clicked');
-   })
+//    tabella.addEventListener('click',function() {
+//       console.log(this);
+//       this.classList.add('actived');
+//    })
 
 
-}
+
+   
+   for(i = 0; i < 100; i++){
+      
+      const quadratino = createSquare(container);
+
+      quadratino.addEventListener('click',function() {
+         console.log(this);
+         this.classList.add('clicked');
+      })
+
+   }
+   
+// }
+   
 
 
 
 function createSquare(container) {
    const quadratino = document.createElement('div');
    quadratino.className = ('quadrato');
+
    container.append(quadratino);
    quadratino.append(i + 1);
+
    return quadratino;
 }
