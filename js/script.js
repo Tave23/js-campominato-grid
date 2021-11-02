c94103bcad0a9a352d10a68fb484bce014f825e8
Davide Tavella <tavella01@gmail.com>
2021-11-02 23:03:30 +0100

    genera una tabella se premi play

--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
 
 const container = document.querySelector('.container');
-// const gamespace = document.getElementById('gamespace');
 let valoreScelta = document.getElementsByClassName('play-button').value;
+const letsPlay = document.querySelector('#gioca')
 
 
-if (valoreScelta === 1) {
-   document.getElementsByClassName('quadrato').style.width = 'calc(100% / 10)';
-   document.getElementsByClassName('quadrato').style.height = 'calc(100% / 10)';
-}
+letsPlay.addEventListener('click',function() {
+   init();
+})
 
-if (valoreScelta === 2) {
-   document.getElementsByClassName('quadrato').style.width = 'calc(100% / 9)';
-   document.getElementsByClassName('quadrato').style.height = 'calc(100% / 9)';
-}
+function init(tot){
+   for(i = 0; i < 100; i++){
+         
+      const quadratino = createSquare(container);
 
-if (valoreScelta === 3) {
-   document.getElementsByClassName('quadrato').style.width = 'calc(100% / 7)';
-   document.getElementsByClassName('quadrato').style.height = 'calc(100% / 7)';
-}
+      quadratino.addEventListener('click',function() {
+      this.classList.add('clicked');
+      })
+
+   }
+
+   hideTitle();
+
+   function hideTitle() {
+      let titolo = document.getElementById('title');
+      titolo.classList.add('hidden');
+   }
 
 
-   
-for(i = 0; i < 100; i++){
+   function createSquare(container) {
+      const quadratino = document.createElement('div');
+      quadratino.className = ('quadrato');
+
+      container.append(quadratino);
+      quadratino.append(i + 1);
+
+      if (valoreScelta === "easy") {
+         document.getElementsByClassName('quadratino').style.width = 'calc(100% / 10)';
+         document.getElementsByClassName('quadratino').style.height = 'calc(100% / 10)';
+      }
       
-   const quadratino = createSquare(container);
+      if (valoreScelta === "hard") {
+         document.getElementsByClassName('quadratino').style.width = 'calc(100% / 9)';
+         document.getElementsByClassName('quadratino').style.height = 'calc(100% / 9)';
+      }
+      
+      if (valoreScelta === "crazy") {
+         document.getElementsByClassName('quadratino').style.width = 'calc(100% / 7)';
+         document.getElementsByClassName('quadratino').style.height = 'calc(100% / 7)';
+      }
 
-   quadratino.addEventListener('click',function() {
-   this.classList.add('clicked');
-   })
-
-}
-   
-
-   
-
-
-
-function createSquare(container) {
-   const quadratino = document.createElement('div');
-   quadratino.className = ('quadrato');
-
-   container.append(quadratino);
-   quadratino.append(i + 1);
-
-   return quadratino;
+      return quadratino;
+   }
 }
